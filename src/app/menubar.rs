6376ec3b816cb92e597a07c0664d2e7e8b5531ad
Alexandra Stephens <redpawa@gmail.com>
2023-02-26 17:45:34 +1300

    Changed home icons

--- src/app/menubar.rs
+++ src/app/menubar.rs
@@ -1,73 +1,86 @@
 use super::images;
 
 pub fn show_menu_bar(parent: &mut super::App, ctx: &egui::Context, frame: &mut eframe::Frame) -> egui::InnerResponse<()> {
+	
+	let home = super::images::StaticSvg::new(
+		String::from("Home"), parent.images.home.clone())
+		.get(ctx);
+
+	let dark_mode = super::images::StaticSvg::new_precalculated(
+		String::from("light-dark-toggle"),
+		parent.images.toggle_off.clone(),
+		parent.images.toggle_on.clone())
+		.get(ctx);
+
+	let vert = super::images::StaticSvg::new(
+		String::from("vert"),
+		parent.images.more_vert.clone())
+		.get(ctx);
+
+	let account = super::images::StaticSvg::new(
+		String::from("has_account"),
+		parent.images.account.clone())
+		.get(ctx);
 
 	let response =
 	egui::TopBottomPanel::top("Menu Bar").show(ctx, |ui| {
 
 		egui::menu::bar(ui, |ui| {
-			ui.menu_button("File", |ui| {
 
-				// Close button: Innactive on web
-				#[cfg(not(target_arch = "wasm32"))]
-				if ui.add(
-					egui::Button::new("Close")
-					.shortcut_text( egui::Context::format_shortcut(ctx, &parent.shortcuts.shutdown))
-					
-				).clicked() {
-					frame.close()
-				}
+			ui.menu_image_button(home.0, home.1, |_ui| {
 
-				// Save button
-				if ui.add(
-					egui::Button::new("Save")
-					.shortcut_text( egui::Context::format_shortcut(ctx, &parent.shortcuts.save))
-
-				).clicked() {
-					if let Some(storage) = frame.storage_mut() {
-						eframe::App::save(parent, storage);
-						ui.close_menu();
-					}
-				}
-
-				// Load button
-				if ui.add(
-					egui::Button::new("Load")
-
-				).clicked() {
-
-					parent.loader.file_dialog(super::loader::FileUsage::ProfilePicture);
-
-					ui.close_menu();
-				}					
 			});
 		
-			// Right hand side
-			ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
+	// Right hand side
+			ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {	
 
-				let has_account = super::images::StaticSvg::new(
-					String::from("has_account"),
-					parent.images.account.clone(),
-					// std::fs::read("assets\\account.svg").unwrap(),
+				ui.menu_image_button(vert.0, vert.1, |ui| {
 
-					);
-
-				let no_account = super::images::StaticSvg::new(
-					String::from("no_account"),
-					parent.images.account_none.clone(),
-					// std::fs::read("assets\\account_none.svg").unwrap(),
-					);
-
-				let (id, size) = match &mut parent.current_user {
-					super::user::CurrentUser::LoggedIn(_) => {
-						has_account.get(ctx, ctx.style().visuals.dark_mode)
+					if ui.add(
+						egui::ImageButton::new(
+							dark_mode.0,
+							dark_mode.1,
+						)
+					).clicked() {
+						if ctx.style().visuals.dark_mode {
+							ctx.set_visuals(egui::Visuals::light())
+						} else {
+							ctx.set_visuals(egui::Visuals::dark())
+						}
 					}
-					super::user::CurrentUser::Empty(_) => {
-						no_account.get(ctx, ctx.style().visuals.dark_mode)
+
+				// Close button: Innactive on web
+					#[cfg(not(target_arch = "wasm32"))]
+					if ui.add(
+						egui::Button::new("Close")
+						.shortcut_text( egui::Context::format_shortcut(ctx, &parent.shortcuts.shutdown))
+					).clicked() {
+						frame.close()
 					}
-				};
-				
-				ui.menu_image_button(id, size,|ui| {
+
+				// Save button
+					if ui.add(
+						egui::Button::new("Save")
+						.shortcut_text( egui::Context::format_shortcut(ctx, &parent.shortcuts.save))
+					).clicked() {
+						if let Some(storage) = frame.storage_mut() {
+							eframe::App::save(parent, storage);
+							ui.close_menu();
+						}
+					}
+
+				// Load button
+					if ui.add(
+						egui::Button::new("Load")
+					).clicked() {
+						parent.loader.file_dialog(super::loader::FileUsage::ProfilePicture);
+						ui.close_menu();
+					}		
+
+				});
+
+										
+				ui.menu_image_button(account.0, account.1,|ui| {
 					match &mut parent.current_user {
 		// User is logged in
 						super::user::CurrentUser::LoggedIn(user) => {
@@ -75,15 +88,14 @@ pub fn show_menu_bar(parent: &mut super::App, ctx: &egui::Context, frame: &mut e
 								ui.label(user.username.clone());
 								ui.label(egui::RichText::new("Currently logged in").weak());
 
-								let (id, size) = user.get_profile_picture(ctx).unwrap_or_else( ||
+								let profile = user.get_profile_picture(ctx).unwrap_or_else( ||
 									images::StaticSvg::new(
-										String::from("no profile picture"),
-										parent.images.no_profile_picture.clone())
-									.get(ctx, ctx.style().visuals.dark_mode)
-								);
+									String::from("no profile picture"),
+									parent.images.no_profile_picture.clone())
+									.get(ctx));
 
 								if ui.add(
-									egui::widgets::ImageButton::new(id, size)
+									egui::widgets::ImageButton::new(profile.0, profile.1)
 								)
 								.on_hover_text("Double click to edit")
 								.double_clicked() {
@@ -118,27 +130,7 @@ pub fn show_menu_bar(parent: &mut super::App, ctx: &egui::Context, frame: &mut e
 					}
 				});
 
-				let light_dark_toggle = super::images::StaticSvg::new_precalculated(
-					String::from("light-dark-toggle"),
-						parent.images.toggle_off.clone(),
-						parent.images.toggle_on.clone(),
-					// std::fs::read("assets\\toggle_off_FILL0.svg").unwrap(),
-					// std::fs::read("assets\\toggle_on_FILL1.svg").unwrap(),	
-				);
-				let (button_id, size) = light_dark_toggle.get(ctx, ctx.style().visuals.dark_mode);
 
-				if ui.add(
-					egui::ImageButton::new(
-						button_id,
-						size
-					)
-				).clicked() {
-					if ctx.style().visuals.dark_mode {
-						ctx.set_visuals(egui::Visuals::light())
-					} else {
-						ctx.set_visuals(egui::Visuals::dark())
-					}
-				}
 			});
 		});
 	});
